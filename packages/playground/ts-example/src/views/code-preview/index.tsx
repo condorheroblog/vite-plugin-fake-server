a5410a087884ab2dc641aaf823e4c9ea197129dc
CondorHero <love2xinwei@gmail.com>
2023-10-15 16:48:54 +0800

    style: add background for textarea

--- packages/playground/ts-example/src/views/code-preview/index.tsx
+++ packages/playground/ts-example/src/views/code-preview/index.tsx
@@ -7,9 +7,9 @@ interface CodePreviewProps {
 
 export function CodePreview({ code, element }: CodePreviewProps) {
 	return (
-		<div style={{ display: "flex", gap: 10 }}>
-			<textarea value={code} readOnly rows={30} cols={50} />
+		<>
+			<textarea className="code-textarea" value={code} readOnly rows={30} cols={50} />
 			<div>{element}</div>
-		</div>
+		</>
 	);
 }
