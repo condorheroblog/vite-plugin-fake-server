4643f6c952292c99d804d585be7e292da6aa7058
CondorHero <love2xinwei@gmail.com>
2023-10-30 20:44:28 +0800

    feat: add and unlink event for fake folder

--- packages/vite-plugin-fake-server/src/node/parallelLoader.ts
+++ packages/vite-plugin-fake-server/src/node/parallelLoader.ts
@@ -1,16 +1,22 @@
+import type { FakeRoute } from "../node";
+
 export function parallelLoader<T>(promises: (() => PromiseLike<T>)[], limit = Infinity) {
 	let current = 0;
-	const result: T[] = [];
+	const result: FakeRoute[] = [];
 	let resolvedCount = 0;
 	const len = promises.length;
-	return new Promise<T[]>((resolve, reject) => {
+	return new Promise<FakeRoute[]>((resolve, reject) => {
 		function processNext() {
 			const index = current;
 			current = index + 1;
 			const promise = promises[index];
 			Promise.resolve(promise())
 				.then((res) => {
-					result[index] = res;
+					if (Array.isArray(res)) {
+						result.push(...res);
+					} else {
+						result.push(res as FakeRoute);
+					}
 					resolvedCount = resolvedCount + 1;
 
 					if (resolvedCount === len) {
